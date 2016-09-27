a40f7fc16fb368a1a4544395aa109585a335225d
Katie Russ <katie@cuttlesoft.com>
2016-09-26 22:08:39 -0400

    Adding base layout

--- app/views.py
+++ app/views.py
@@ -9,36 +9,36 @@ from .models import User
 
 
 @app.route('/')
-def hello_world():
-    return render_template('hello_world.html', current_user=current_user)
+def index():
+    return render_template('index.html', current_user=current_user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        # flash('You are already logged in.')
+        flash('You are already logged in.')
         return redirect('/')
 
     form = LoginForm()
     if form.validate_on_submit():  # includes username/password validations
         user = User.query.filter_by(username=form.username.data).first()
         login_user(user)
-        # flash('Logged in successfully.')
+        flash('Logged in successfully.')
         return redirect('/')
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, title='Log In')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
-        # flash('You are already logged in.')
+        flash('You are already signed up.')
         return redirect('/')
 
     form = SignupForm()
     if form.validate_on_submit():  # includes username/password validations
         user = User(username=form.username.data, password=form.password.data).save()
         login_user(user)
-        # flash('Signed up successfully.')
+        flash('Signed up successfully.')
         return redirect('/')
     return render_template('signup.html', form=form)
 
@@ -49,5 +49,5 @@ def signup():
 @login_required
 def logout():
     logout_user()
-    # flash('Logged out successfully.')
+    flash('Logged out successfully.')
     return redirect('/')
